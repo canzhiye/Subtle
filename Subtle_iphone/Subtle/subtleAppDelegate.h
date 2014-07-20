5feb9c6c21f904a5086523650ef99afc8d1fe639
Coulton Vento <coultonvento@gmail.com>
2014-07-19 19:55:40 -0700

    Added iPhone XCode proj

---
+++ Subtle_iphone/Subtle/subtleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  subtleAppDelegate.h
+//  Subtle
+//
+//  Created by Coulton Vento on 7/19/14.
+//  Copyright (c) 2014 Subtle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface subtleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Subtle_iphone/Subtle/subtleViewController.h
@@ -0,0 +1,13 @@
+//
+//  subtleViewController.h
+//  Subtle
+//
+//  Created by Coulton Vento on 7/19/14.
+//  Copyright (c) 2014 Subtle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface subtleViewController : UIViewController
+
+@end
